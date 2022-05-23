dec083e5778cdaed6c17b2c7aead91636da192ca
Paweł Niedźwiecki <hello@uxu.pl>
2022-05-23 22:57:27 +0200

    i added sendgird for email

--- config/plugins.js
+++ config/plugins.js
@@ -10,4 +10,17 @@ module.exports = ({ env }) => ({
       },
     },
   },
+
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: env("SENDGRID_EMAIL_FROM"),
+        defaultReplyTo: env("SENDGRID_EMAIL_TO"),
+      },
+    },
+  },
 });
